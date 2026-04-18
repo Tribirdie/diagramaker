f395cd118f01dc07a5957c385cfcb47a06b88513
Estudiante <apaapf23@gmail.com>
2026-04-18 17:59:41 -0300

    nodes can now be resized

--- stuff/CircleNode.jsx
+++ stuff/CircleNode.jsx
@@ -1,14 +1,13 @@
 import {useCallback, memo} from 'react';
-import {Position, Handle} from '@xyflow/react';
+import {Position, Handle, NodeResizer} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
-const CircleNode = memo(({data}) =>{
+const CircleNode = memo(({selected}) =>{
 	return (
 		<div id="circlenode">
-		<p>
-		<input id="input"></input>
-		</p>
-
+		<NodeResizer isVisible={selected} color="#304b6b">
+		</NodeResizer>
+		<textarea id="input2"></textarea>
 		<Handle type='source' position={Position.Bottom} id="bottom"/>
 		<Handle type='target' position={Position.Top} id="top"/>
 	
--- stuff/RectangularNode.jsx
+++ stuff/RectangularNode.jsx
@@ -1,18 +1,20 @@
 import {useCallback, memo} from 'react';
-import {Position, Handle} from '@xyflow/react';
+import {Position, Handle, NodeResizer} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
-const RectangularNode = memo(() =>{
+const RectangularNode = memo(({selected}) =>{
 	return (
-		<div id="rectangle"> 
-		<input id="input"></input>
-
+		<div id="rectangle">
+		<NodeResizer isVisible={selected} color="#304b6b">
+		</NodeResizer>
+		<textarea id="input"></textarea>
 		<Handle type='target' position={Position.Top} id="top"/>
 		<Handle type='target' position={Position.Left} id="left"/>
 
 		<Handle type='source' position={Position.Bottom} id="bottom"/>
 		<Handle type='source' position={Position.Right} id="right"/>
 		</div>
+
 	)
 })
 
